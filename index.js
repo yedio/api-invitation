7476538d221b70e69383729ccc6b59010f119740
yedio <yediobjet@gmail.com>
2025-03-09 16:20:40 +0900

    Node: 3.2 라우터 만들기

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const http = require("http");
+const url = require("url"); //url 모듈 로딩
 
 const server = http.createServer((req, res) => {
-  // 응답의 헤더 설정
+  const path = url.parse(req.url, true).pathname;
   res.setHeader("Content-Type", "text/html");
 
-  // "OK"를 응답하고 종료
-  res.end("OK");
+  if (path === "/user") {
+    res.end("[user] name : andy, age: 30");
+  } else if (path === "/feed") {
+    res.end(`
+      <ul>
+      <li>picture1</li>
+      <li>picture2</li>
+      <li>picture3</li>
+      </ul>
+      `);
+  } else {
+    res.statusCode = 404;
+    res.end("404 page not found");
+  }
 });
 
 // 서버 시작, 접속 대기
