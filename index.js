eb87da8a5204eab7b49015dc0b502bab05da97e1
yedio <yediobjet@gmail.com>
2025-03-09 16:40:09 +0900

    Node: 라우터 동적처리 및 리팩토링

--- index.js
+++ index.js
@@ -5,21 +5,40 @@ const server = http.createServer((req, res) => {
   const path = url.parse(req.url, true).pathname;
   res.setHeader("Content-Type", "text/html");
 
-  if (path === "/user") {
-    res.end("[user] name : andy, age: 30");
-  } else if (path === "/feed") {
-    res.end(`
+  if (path in urlMap) {
+    urlMap[path](req, res);
+  } else {
+    notFound(req, res);
+  }
+});
+
+// 서버 시작, 접속 대기
+server.listen(3000, () => console.log("OK!"));
+
+const user = (req, res) => {
+  //라우터 동적 처리
+  const userInfo = url.parse(req.url, true).query; 
+  res.end(`[user] name : ${userInfo?.name}, age: ${userInfo?.age}`);
+};
+
+const feed = (req, res) => {
+  res.end(`
       <ul>
       <li>picture1</li>
       <li>picture2</li>
       <li>picture3</li>
       </ul>
       `);
-  } else {
-    res.statusCode = 404;
-    res.end("404 page not found");
-  }
-});
+};
 
-// 서버 시작, 접속 대기
-server.listen(3000, () => console.log("OK!"));
+const notFound = (req, res) => {
+  res.statusCode = 404;
+  res.end("404 page not found");
+};
+
+//라우터 규칙
+const urlMap = {
+  "/": (req, res) => res.end("HOME"),
+  "/user": user,
+  "/feed": feed,
+};
